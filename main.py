2e7f363e95d49f232aaa1c32b4b518b6cd56a2ca
chizuchizu <ochimasumi@gmail.com>
2019-08-28 19:15:30 +0900

    good

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from face_detection import detection
 from make_rectangle import make
 
@@ -7,6 +8,7 @@ if __name__ == '__main__':
     ESC_KEY = 27  # Escキー
     INTERVAL = 33  # 待ち時間
     FRAME_RATE = 1  # fps
+    time_limit = 30  # limit
 
     ORG_WINDOW_NAME = "org"
     GAUSSIAN_WINDOW_NAME = "gaussian"
@@ -39,6 +41,11 @@ if __name__ == '__main__':
     color = (200, 200, 200)
     memo = 3
 
+    time_1 = time.time()
+    a = time_1
+
+    count = b = 0
+
     # 変換処理ループ
     while end_flag:
         # 画像の取得と顔の検出
@@ -47,14 +54,17 @@ if __name__ == '__main__':
         if face_flag:
             rec = make(H, W)
             face_flag = False
+            b = time.time() - a
+            a = time.time()
+            count += 1
 
         x, y, w, h = rec[0], rec[1], rec[2], rec[3]
         color = (0, 200, 225)
         pen_w = 3
         cv2.rectangle(img2, (x, y), (w, h), color, thickness=pen_w)
 
-        # フレーム表示
-        # cv2.imshow(ORG_WINDOW_NAME, img2)
+        cv2.putText(img2, "{0: .2f}sec  {1}count".format(b, count), (20, 60), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=2,
+                    color=(0, 0, 0), thickness=6)
 
         img3, face_list = detection(img2, cascade)
         # print(face_list)
@@ -64,11 +74,12 @@ if __name__ == '__main__':
                     and face_list[0][3] + face_list[0][1] < h:
                 face_flag = True
 
+        # Frame
         cv2.imshow(ORG_WINDOW_NAME, img3)
 
-        # Escキーで終了
+        # Escキーで終了 or Time Limit
         key = cv2.waitKey(INTERVAL)
-        if key == ESC_KEY:
+        if key == ESC_KEY or time.time() - time_1 > time_limit:
             break
 
         # 次のフレーム読み込み
