bfc14eff5cab5f78a3c5737d2c57668d15a2b756
chizuchizu <ochimasumi@gmail.com>
2019-09-19 23:00:53 +0900

    Add commandline argument(ミス修正)

--- core/main.py
+++ core/main.py
@@ -35,8 +35,7 @@ class Main:
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("-g", "--gpu", action="store_true", help="TF gpu版を使用しているかどうか")
         self.args = self.parser.parse_args()
-        print(self.args.gpu)
-        if gpu:
+        if self.args.gpu:
             """gpuを使用する場合はgpuの指定をしなければならないので"""
             from core.face_emotions import use_gpu
             use_gpu()
